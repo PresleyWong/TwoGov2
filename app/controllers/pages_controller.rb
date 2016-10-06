a0e2658ec84eb08b32097d89c570cc4bd6b9ec05
ivanseven <sir.ivanyeong@gmail.com>
2016-10-07 04:40:19 +0800

    Added Form to HomePage and adjusted pg_search columns

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 	def search
   	# place in postings controller?
-	    search = "#{params[:search_location]} #{params[:search_activity]}"
+  	byebug
+	    search = "#{params[:location]} #{params[:activity]}"
 	    @posts = Post.search_by_columns(search)
 	  	render "posts/index"
 	end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,5 @@ class Post < ActiveRecord::Base
 
     validates :activity_type, :language_type, :duration_type, :description, :address, presence: true
 
-	pg_search_scope :search_by_columns, :against => [:location, :activity_type]
+	pg_search_scope :search_by_columns, :against => [:activity_type, :address]
 end
