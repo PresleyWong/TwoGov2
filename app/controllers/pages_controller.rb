f58ee99711aa0f6e1cdd7ee8a9810e40ebde04b0
ivanseven <sir.ivanyeong@gmail.com>
2016-10-07 05:57:48 +0800

    Removed Byebug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
 	def search
   	# place in postings controller?
-  	byebug
 	    search = "#{params[:location]} #{params[:activity]}"
 	    @posts = Post.search_by_columns(search)
 	  	render "posts/index"
