dec4604703e4847a453668150fa2dab6a3ce5f42
ivanseven <sir.ivanyeong@gmail.com>
2016-10-06 23:53:26 +0800

    Added pg_search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,8 @@
 class PagesController < ApplicationController
+	def search
+  	# place in postings controller?
+	    search = "#{params[:search_location]} #{params[:search_activity]}"
+	    @posts = Post.search_by_columns(search)
+	  	render "posts/index"
+	end
 end
