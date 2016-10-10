56da519a6563843462116d72670760be22bc0081
ivanseven <sir.ivanyeong@gmail.com>
2016-10-11 04:20:10 +0800

    Halfway Fixing Pg_search for multiple values

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,23 @@
 class PagesController < ApplicationController
 	def search
-    if !params[:activity].nil?
-      activities = params[:activity].join(" ")
+    start = Time.now
+    # if !params[:activity].nil?
+    #   activities = params[:activity].join(" ")
+    # end
+    # if !params[:language].nil?
+    #   languages = params[:language].join(" ")
+    # end
+    # if !params[:time].nil?
+    #   times = params[:time].join(" ")
+    # end
+    @posts = Post.all
+    params[:activity].each do |activity|
+	   @posts += Post.search_by_activities(activity)
     end
-    if !params[:language].nil?
-      languages = params[:language].join(" ")
-    end
-    if !params[:time].nil?
-      times = params[:time].join(" ")
-    end
-	    search = "#{params[:location]} #{activities} #{languages} #{times}"
-      if search == "   "
-       @posts = Post.all
-      else
-	     @posts = Post.search_by_columns(search)
-	    end
       # @posts.users.where(gender:params[:gender], age:params[:age])
 	    @activities_bar = Activity.all
+          end_time = Time.now
+      @time = end_time - start
 	  	render "posts/index"
 	end
 end
