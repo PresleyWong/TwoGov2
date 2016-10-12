0d319eac78e535f3c21d308402a4b1b734fa792f
nextacademy <presley_wg@yahoo.com>
2016-10-12 10:34:53 +0800

    updating mailbox

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,23 @@
 
 Geocoder.configure(
-  api_key: ENV["GEOCODING_API_ID"] 
+
+  # geocoding service (see below for supported options):
+  :lookup => :yandex,
+
+  # IP address geocoding service (see below for supported options):
+  :ip_lookup => :maxmind,
+
+  # to use an API key:
+  :api_key => ENV["GEOCODING_API_ID"] ,
+
+  # geocoding service request timeout, in seconds (default 3):
+  :timeout => 5,
+
+  # set default units to kilometers:
+  :units => :km,
+
+  # caching (see below for details):
+  :cache => Redis.new,
+  :cache_prefix => "..."
+
 )
