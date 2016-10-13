f6e25f3e93e1ad60ece334ff9a1a6e03d8c1d29a
nextacademy <presley_wg@yahoo.com>
2016-10-13 10:36:33 +0800

    changes in review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update]
 
 	def show
-		@review = Review.where(post_id: current_user.id)
+		@review = Review.where(post_id: @user.id)
 	end
 
 	def buddy
